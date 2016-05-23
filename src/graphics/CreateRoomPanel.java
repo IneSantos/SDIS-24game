cd4dbcd99d16abb9c60bd5cca49aff6d882f0a22
IneSantos <inesscarneiro94@gmail.com>
2016-05-23 15:11:20 +0100

    Problem fixed in Enter name and nickname.... bug in the other panel... fixing it

--- src/graphics/CreateRoomPanel.java
+++ src/graphics/CreateRoomPanel.java
@@ -7,46 +7,46 @@ import java.awt.event.*;
 /**
  * Created by inesa on 22/05/2016.
  */
-public class CreateRoomPanel extends JPanel{
+public class CreateRoomPanel extends JPanel {
 
     private static final int PREF_W = 400;
     private static final int PREF_H = 400;
     JLabel jlabel;
     JLabel jlabel1;
     JButton create;
-    JTextField  textField;
-    JTextField  textField1;
-    String  nickName = "";
-    String  roomName = "";
+    JTextField textField;
+    JTextField textField1;
+    String nickName = "";
+    String roomName = "";
 
     public CreateRoomPanel() {
         setPreferredSize(new Dimension(PREF_W, PREF_H));
         //setBorder(BorderFactory.createLineBorder(Color.blue));
-        jlabel  = new JLabel("Create Room: ");
-        jlabel.setFont(new Font("Verdana",1,30));
-        jlabel.setPreferredSize(new Dimension(300,30));
+        jlabel = new JLabel("Create Room: ");
+        jlabel.setFont(new Font("Verdana", 1, 30));
+        jlabel.setPreferredSize(new Dimension(300, 30));
         add(jlabel);
 
         add(Box.createVerticalStrut(80));
 
-        jlabel1  = new JLabel("Room name: ");
-        jlabel1.setFont(new Font("Verdana",1,15));
-        jlabel1.setPreferredSize(new Dimension(130,30));
+        jlabel1 = new JLabel("Room name: ");
+        jlabel1.setFont(new Font("Verdana", 1, 15));
+        jlabel1.setPreferredSize(new Dimension(130, 30));
 
         //enterText
         textField1 = new JTextField("Enter name...", 20);
-        textField1.setFont(new Font("Verdana",4,10));
+        textField1.setFont(new Font("Verdana", 4, 10));
 
 
         add(jlabel1);
         add(textField1);
 
-        jlabel  = new JLabel("Your nickname: ");
-        jlabel.setFont(new Font("Verdana",1,15));
+        jlabel = new JLabel("Your nickname: ");
+        jlabel.setFont(new Font("Verdana", 1, 15));
 
         //enterText
         textField = new JTextField("Enter nickname...", 20);
-        textField.setFont(new Font("Verdana",4,10));
+        textField.setFont(new Font("Verdana", 4, 10));
 
         add(jlabel);
         add(textField);
@@ -55,52 +55,29 @@ public class CreateRoomPanel extends JPanel{
         keyboardListenners();
     }
 
-    public void mouseListenners(){
+    public void mouseListenners() {
 
-        textField1.addMouseListener(new MouseAdapter(){
+        textField1.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e){
+            public void mouseClicked(MouseEvent e) {
                 textField1.setText("");
+                if (textField.getText().equals(""))
+                    textField.setText("Enter nickname...");
             }
-
-            @Override
-            public void mouseExited(MouseEvent e){
-                if(textField1.getText().equals(""))
-                    textField1.setText("Enter Name..");
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                if(textField1.getText().equals(""))
-                    textField1.setText("Enter Name..");
-
-            }
-
-
         });
 
-        textField.addMouseListener(new MouseAdapter(){
+        textField.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e){
+            public void mouseClicked(MouseEvent e) {
                 textField.setText("");
-            }
-            @Override
-            public void mouseExited(MouseEvent e){
-                if(textField.getText().equals(""))
-                    textField.setText("Enter nickname...");
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                if(textField.getText().equals(""))
-                    textField.setText("Enter nickname...");
-
+                if (textField1.getText().equals(""))
+                    textField1.setText("Enter Name..");
             }
         });
     }
 
 
-    public void keyboardListenners(){
+    public void keyboardListenners() {
 
         textField1.addKeyListener(new KeyAdapter() {
             @Override
@@ -112,7 +89,7 @@ public class CreateRoomPanel extends JPanel{
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
                 char c = e.getKeyChar();
-                roomName += c ;
+                roomName += c;
                 System.out.println(roomName);
             }
 
@@ -133,7 +110,7 @@ public class CreateRoomPanel extends JPanel{
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
                 char c = e.getKeyChar();
-                nickName += c ;
+                nickName += c;
                 System.out.println(nickName);
             }
 
--- src/graphics/JoinRoomPanel.java
+++ src/graphics/JoinRoomPanel.java
@@ -1,11 +1,15 @@
 package graphics;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.Document;
 import javax.swing.text.html.CSS;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -15,7 +19,7 @@ import java.util.ArrayList;
  */
 public class JoinRoomPanel extends JPanel {
 
-    private static final int PREF_W = 500;
+    private static final int PREF_W = 600;
     private static final int PREF_H = 400;
     JLabel jlabel;
     ArrayList<String> roomsName = new ArrayList<>();
@@ -25,55 +29,69 @@ public class JoinRoomPanel extends JPanel {
     public JoinRoomPanel() {
 
         roomsName.add("teste0000000000000000000000000000000000000000000000000000000000000000");
-        roomsName.add("teste10000000000000000000000000000000000000000000000000000000000000000");
-        roomsName.add("teste20000000000000000000000000000000000000000000000000000000000000000");
-        roomsName.add("teste30000000000000000000000000000000000000000000000000000000000000000");
-        roomsName.add("teste40000000000000000000000000000000000000000000000000000000000000000");
+        roomsName.add("teste1000000000000000000000000000000000000000000000000000000000000000");
+        roomsName.add("teste2000000000000000000000000000000000000000000000000000000000000000");
+        roomsName.add("teste3000000000000000000000000000000000000000000000000000000000000000");
+        roomsName.add("teste4000000000000000000000000000000000000000000000000000000000000000");
 
         setPreferredSize(new Dimension(PREF_W, PREF_H));
         setBorder(BorderFactory.createLineBorder(Color.blue));
 
-        jlabel  = new JLabel("Join Room: ");
-        jlabel.setFont(new Font("Verdana",1,30));
-        jlabel.setPreferredSize(new Dimension(400,30));
+        jlabel = new JLabel("Join Room: ");
+        jlabel.setFont(new Font("Verdana", 1, 30));
+        jlabel.setPreferredSize(new Dimension(400, 30));
         add(jlabel);
 
         add(Box.createVerticalStrut(80));
 
-        JTextPane textArea = new JTextPane();
-        textArea.setPreferredSize(new Dimension(400,300));
-        textArea.setEditable(false);
+        //  JPanel panel = new JPanel(new FlowLayout());
 
-        JScrollPane areaScroll = new JScrollPane(textArea);
+        JTextPane pane = new JTextPane();
+        pane.setEditable(false);
+
+        JScrollPane areaScroll = new JScrollPane(pane);
+        areaScroll.setPreferredSize(new Dimension(500, 300));
+        areaScroll.setViewportBorder(new LineBorder(Color.RED));
         areaScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        areaScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        add(areaScroll);
+        //  areaScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-        for(int i = roomsName.size() ; i > 0; i--){
-            JLabel roomName = new JLabel("1",JLabel.CENTER);
-            roomName.setText("<html>" + roomsName.get(i-1) + "</html>");
-            roomName.setFont(new Font("Verdana",4,20));
-            /*Dimension d = roomName.getPreferredSize();
-            System.out.println("width " + d.width);
-            System.out.println("height "  + d.height);
-            roomName.setPreferredSize(new Dimension(d.width,d.height));*/
-            roomName.setBorder(BorderFactory.createLineBorder(Color.blue));
-            textArea.insertComponent(roomName);
 
-            roomName.addMouseListener(new MouseAdapter() {
+     /*   JPanel jPanel = new JPanel();
+        for(int i = roomsName.size() - 1 ; i >= 0; i--) {
+            JLabel label = new JLabel();
+            label.setText(roomsName.get(i));
+            jPanel.add(label);
+            jPanel.add(Box.createRigidArea(new Dimension(1, 5)));
+*/
+           /* label.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseEntered(MouseEvent e) {
-                    roomName.setForeground(Color.BLUE);
+                    label.setForeground(Color.BLUE);
 
                 }
 
                 @Override
                 public void mouseExited(MouseEvent e) {
-                    roomName.setForeground(Color.BLACK);
+                    label.setForeground(Color.BLACK);
 
                 }
             });
+        }*/
+     /*   BoxLayout boxLayout = new BoxLayout(jPanel, BoxLayout.Y_AXIS);
+        jPanel.setLayout(boxLayout);
+        add(areaScroll);
+*/
+        JPanel jPanel = new JPanel();
+        for (int i = 0; i < 50; i++) {
+            JLabel jLabel = new JLabel("Label");
+            jPanel.add(jLabel);
+            jPanel.add(Box.createRigidArea(new Dimension(1, 5)));
         }
+        BoxLayout boxLayout = new BoxLayout(jPanel, BoxLayout.Y_AXIS);
+        jPanel.setLayout(boxLayout);
+        JScrollPane jScrollPane = new JScrollPane(jPanel);
+        add(jScrollPane);
+        // }
 
     }
 
